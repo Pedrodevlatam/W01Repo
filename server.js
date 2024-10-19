ae91e65234994a0e4f978765f1f53dd333a66726
Pedrodevlatam <pamp1891@gmail.com>
2024-10-19 15:49:37 -0500

    w03update

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute.js");
+const utilities = require("./utilities/index.js");
+const intentionalErrorRoute = require("./routes/intentionalErrorRoute.js");
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -24,8 +27,37 @@ app.set("layout", "./layouts/layout"); // Not at view root
 app.use(static)
 
 // Index route
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
+app.get("/", utilities.handleErrors(baseController.buildHome));
+
+// Inventory routes
+app.use("/inv", inventoryRoute);
+
+// Intentional error route
+app.use("/ierror", intentionalErrorRoute);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'});
+});
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  console.dir(err);
+  if (err.status == 404) { 
+    message = err.message; 
+  } else { 
+    message = 'Oh no! There was a crash. Maybe try a different route?'; 
+  }
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  });
 });
 
 /* ***********************
